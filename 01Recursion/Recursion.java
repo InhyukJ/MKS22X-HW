57809dd6963d37409d37d27cf20b028c674e5088
InhyukJ <ijun@stuy.edu>
2017-02-06 21:59:29 -0500

    Unrevised version completed

---
+++ 01Recursion/Recursion.java
@@ -0,0 +1,29 @@
+public class Recursion {
+    public static String name() {
+	return "Jun,Inhyuk";
+    }
+
+    public static double sqrt(double n) {
+	if (n <= 0) {
+	    throw new IllegalArgumentException("You cannot take a square root of a negative number!");
+	}
+	sqrtHelper(n, 1.0);
+    }
+
+    private static double sqrtHelper(double n, double guess) {
+	if (isCloseEnough((guess * guess), n)) {
+	    return guess;
+	}
+	else {
+	    int better_guess = (n / guess + guess) / 2;
+	    sqrtHelper(n, better_guess);
+	}
+    }
+
+    private static boolean isCloseEnough(double a, double b) {
+        double difference = Math.abs(a - b);
+	double average = (a + b) / 2;	
+	double perDifference = difference / average;
+	return (perDifference < 0.00000001); //10^-8%
+    }
+}
