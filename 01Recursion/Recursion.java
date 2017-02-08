0580f45c6b8ceb342a85f60e32bd45a75b2dcafc
InhyukJ <ijun@stuy.edu>
2017-02-07 22:59:36 -0500

    Tested code

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -4,8 +4,11 @@ public class Recursion {
     }
     
     public static double sqrt(double n) {
-	if (n <= 0) {
+	if (n < 0) {
 	    throw new IllegalArgumentException("You cannot take a square root of a negative number!");
+	}
+	if (n == 0.0) {
+	    return 0.0;
 	}
 	return sqrtHelper(n, 1.0);
     }
@@ -26,4 +29,30 @@ public class Recursion {
 	double perDifference = difference / average;
 	return (perDifference < 0.00000001); //10^-8%
     }
+
+    /* Tested code
+    public static void main(String[]args) {
+	System.out.println(name());
+
+	double[] squares = {1.0, 4.0, 9.0, 16.0, 25.0, 36.0, 49.0, 64.0};
+	for (double square : squares) {
+	    System.out.println(sqrt(square));
+	}
+
+	System.out.println("\n");
+	
+	for (double n = 1.0;n < 10.0;n += 1.0) {
+	    System.out.println(sqrt(n));
+	}
+
+	System.out.println(sqrt(0.0));
+	//System.out.println(sqrt(-1.0));
+
+	System.out.println("\n");
+
+	for (double n = 0.0;n < 1.0;n += 0.1) {
+	    System.out.println(sqrt(n));
+	}
+    }
+    */
 }
