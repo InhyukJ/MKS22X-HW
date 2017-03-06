71375ddf6312084cc2d259ed1f45293e4f0eb6b3
InhyukJ <ijun@stuy.edu>
2017-03-06 15:35:16 -0500

    ahh

--- 05x/USACO.java
+++ 05x/USACO.java
@@ -1,4 +1,13 @@
 public class USACO {
-    public int bronze(String filename);
+    public int bronze(String filename) {
+	private int R, C, E, N;
+	private int[][] lake;
+	File file = new File(filename);
+	Scanner in = new Scanner(file);
+	while (in.hasNextLine()) { //parse all into an arraylist and separatethem
+	    String word = in.next();
+	    
+    }
+    
     public int silver(String filename);
 }
