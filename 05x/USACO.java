6f162e8fae1c0e552af634d07d0a87eb22d1ed51
InhyukJ <ijun@stuy.edu>
2017-03-20 19:26:37 -0400

    Fixed Syntactical Issues?

--- 05x/USACO.java
+++ 05x/USACO.java
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-public class USACO {
-    public int bronze(String filename) {
-	private int R, C, E, N;
-	private int[][] lake;
-	File file = new File(filename);
-	Scanner in = new Scanner(file);
-	String[] index = in.nextLine().split(" ");
-	R = index[1];
-	C = index[2];
-	E = index[3];
-	N = index[4];
-	lake = new int[R][C];
-	for (int row = 0;row < R;row++) {
-	    for (int col = 0;col < C;col++) {
-		lake[row][col] = in.next(); //Integer.parseInt() or something
-	    }
-	}
-	while (in.hasNextLine()) {
-	    String[] direction = in.nextLine().split(" ");
-	    command(direction[1], direction[2], direction[3]);
-	}
-
-	return;	    
-    }
-    
-    private void command(int R_s, int C_s, int D_s) {
-	for (int row = R_s;row < R_s + 3;R_s++) {
-	    for (int col = C_s;col < C_s + 3;C_s++) {
-		lake[row][col] 
-    
-    public int silver(String filename);
-=======
 import java.util.*;
 import java.io.*;
 
@@ -164,9 +131,8 @@ public class USACO {
     /*
     public static void main(String[]args) {
 	USACO x = new USACO();
-	//System.out.println(x.bronze("makelake.dat"));
+	System.out.println(x.bronze("makelake.dat"));
 	System.out.println(x.silver("ctravel.dat"));
     }
     */
->>>>>>> 410f3f8f4df410562763a09b8f2128c76e37de25
 }
